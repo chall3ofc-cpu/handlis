39ff782007420805e1d81f7dc9acb26e7099ebb2
Charlie <chall3.ofc@gmail.com>
2026-08-24 19:13:34 +0200

    fix: removed git merge conflict markers from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
-import { base44 } from '@base44/vite' // <-- Kontrollera att denna rad är med!
-=======
 import { base44 } from '@base44/vite'
->>>>>>> 972ccb8 (fix: explicit import of base44 in vite config)
 
 // https://vite.dev
 export default defineConfig({
